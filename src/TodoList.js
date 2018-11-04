246261ab1be0ef7adc490e3280e2da29e34f8aa8
Roman Matveev <roman.x@live.com>
2018-11-04 11:34:35 -0500

    Added delete and toggle functionality.

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 import TodoForm from './TodoForm';
 
-const API_URL = '/api/todos';
+const API_URL = '/api/todos/';
 
 class TodoList extends Component {
     constructor(props) {
@@ -58,10 +58,65 @@ class TodoList extends Component {
         })
     }
 
+    deleteTodo(id) {
+        const deleteURL = API_URL + id;
+
+        fetch(deleteURL, {
+            method: 'delete'
+        }).then(res => {
+            if (!res.ok) {
+                if (res.status >= 400 && res.status < 500) {
+                    return res.json().then(data => {
+                        let err = {errMessage: data.message}
+                        throw err;
+                    });
+                } else {
+                    let err = {errMessage: 'Server is not responding.'}
+                    throw err;
+                }
+            } return res.json();
+        }).then(() => {
+            const todos = this.state.todos.filter(todo => todo._id !== id);
+            this.setState({todos: todos});
+        })
+    }
+
+    toggleTodo(todo) {
+        const updateURL = API_URL + todo._id;
+
+        fetch(updateURL, {
+            method: 'put',
+            headers: new Headers({
+                'content-type': 'application/json'
+            }),
+            body: JSON.stringify({completed: !todo.completed})
+        }).then(res => {
+            if (!res.ok) {
+                if (res.status >= 400 && res.status < 500) {
+                    return res.json().then(data => {
+                        let err = {errMessage: data.message}
+                        throw err;
+                    });
+                } else {
+                    let err = {errMessage: 'Server is not responding.'}
+                    throw err;
+                }
+            } return res.json();
+        }).then(updatedTodo => {
+            const todos = this.state.todos.map((t) => (
+                t._id === updatedTodo._id ? {...t, completed: !t.completed} : t
+            ));
+            this.setState({todos: todos});
+        })
+    }
+
     render() {
         const todos = this.state.todos.map((t) => (
             <TodoItem key={t._id}
                 {...t}
+
+                onDelete={this.deleteTodo.bind(this, t._id)}
+                onToggle={this.toggleTodo.bind(this, t)}
             />
         ));
 
